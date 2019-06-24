e0dbdcc7ecd1289e0ccac095f3d3596e29f3f9d5
oksanahl <oksana.hlushko.canada@gmail.com>
2019-06-23 18:07:26 -0700

    modified some stuff

---
+++ src/main/java/TCPMessage.java
@@ -0,0 +1,23 @@
+public class TCPMessage {
+
+    public String messageType;
+    public String commandType;
+    public String senderIP;
+    public String destinationIP;
+    public long sendTimestamp;
+
+    public String dataList;
+    public String localFileName;
+    public String fs533FileName;
+    public boolean fileSaveConfirm;
+
+    public TCPMessage(String messageType, String commandType, String senderIP, String destinationIP,  long sendTimestamp)
+    {
+        this.commandType = commandType;
+        this.messageType = messageType;
+        this.senderIP = senderIP;
+        this.sendTimestamp = sendTimestamp;
+        this.destinationIP = destinationIP;
+
+    }
+}
